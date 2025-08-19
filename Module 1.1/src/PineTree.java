db640cdb6e3daa6fd20aae2cf485f346135e1fe7
Blendi Grajqevci <blendi.gr@hotmail.fi>
2025-08-19 11:02:17 +0300

    Module 1.1 Ready

---
+++ Module 1.1/src/PineTree.java
@@ -0,0 +1,8 @@
+public class PineTree  {
+    public static void  main(String[] args) {
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("*******");
+    }
+}
